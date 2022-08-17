4ef2dda46d76a7aefd4c4b82da1e6c236d7f3176
Tiashka <tiashkas@agilebridge.co.za>
2022-08-17 10:55:48 +0200

    added soap successfully

---
+++ Booker/Booker/Booker.Api/Controllers/SoapController.cs
@@ -0,0 +1,29 @@
+﻿using Booker.Api.Logic;
+using Microsoft.AspNetCore.Mvc;
+using NumberConversionServiceReference;
+using static Booker.Api.Logic.SoapCallLogic;
+using static NumberConversionServiceReference.NumberConversionSoapTypeClient;
+
+namespace Booker.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SoapController : ControllerBase
+    {
+
+        private readonly ILogger<SoapController> _logger;
+
+        public SoapController(ILogger<SoapController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("{payload}")]
+        public Task<SoapResponse> PostNumberToWordsViaSoap([FromRoute] int payload)
+        {
+            SoapCallLogic scl = new SoapCallLogic();
+            var response = scl.TrySoapCallAsync(payload);
+            return response;
+        }
+    }
+}
---
+++ Booker/Booker/Booker.Api/Logic/SoapCallLogic.cs
@@ -0,0 +1,24 @@
+﻿using NumberConversionServiceReference;
+using static NumberConversionServiceReference.NumberConversionSoapTypeClient;
+
+namespace Booker.Api.Logic
+{
+    public class SoapCallLogic
+    {
+        public async Task<SoapResponse> TrySoapCallAsync(int payload)
+        {
+            EndpointConfiguration endpointConfig = new EndpointConfiguration();
+            var soapClient = new NumberConversionSoapTypeClient(endpointConfig);
+            var response = await soapClient.NumberToWordsAsync(Convert.ToUInt64(payload));
+            return new SoapResponse()
+            {
+                responseWord = response.Body.NumberToWordsResult
+            };
+        }
+
+        public class SoapResponse
+        {
+            public string responseWord { get; set; }
+        }
+    }
+}
