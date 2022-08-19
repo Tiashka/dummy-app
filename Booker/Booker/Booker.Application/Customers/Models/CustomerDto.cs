3f4195994c0676ca0d0722f19844645730905a74
Tiashka <tiashkas@agilebridge.co.za>
2022-08-19 11:46:34 +0200

    added get customers endpoint

---
+++ Booker/Booker/Booker.Application/Customers/Models/CustomerDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booker.Application.Customers.Models
+{
+    public class CustomerDto
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public long Contact { get; set; }
+    }
+}
---
+++ Booker/Booker/Booker.Application/Customers/Queries/GetAllCustomersQuery.cs
@@ -0,0 +1,40 @@
+﻿using Booker.Application.Customers.Models;
+using Booker.Domain;
+using Booker.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booker.Application.Queries
+{
+    public class GetAllCustomersQuery : IRequest<IQueryable<CustomerDto>>
+    {
+    }
+
+    public class GetAllCustomersQueryHandler : IRequestHandler<GetAllCustomersQuery, IQueryable<CustomerDto>>
+    {
+        private readonly BookerDbContext _dbContext;
+
+        public GetAllCustomersQueryHandler(BookerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<IQueryable<CustomerDto>> Handle(GetAllCustomersQuery request, CancellationToken cancellationToken)
+        {
+            List<Customer> customers = _dbContext.Customers.ToList();
+            List<CustomerDto> customerDto = new List<CustomerDto>();
+
+            foreach( var cust in customers)
+            {
+                customerDto.Add(new CustomerDto() { ID = cust.ID, Name = cust.Name, Contact = cust.Contact });
+            }
+            
+            return Task.FromResult(customerDto.AsQueryable());
+        }
+    
+    }
+}
